f1e56cc09e0522386f294f9cd73d44426b3318d9
Omar-bit <Bouassidaomar9@gmail.com>
2026-04-12 17:45:15 +0100

    init

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className='min-h-screen bg-gray-50 flex items-center justify-center'>
+      <h1 className='text-4xl font-bold text-red-600'>
+        Al Malaki - Client Initialized
+      </h1>
+    </div>
+  );
+}
