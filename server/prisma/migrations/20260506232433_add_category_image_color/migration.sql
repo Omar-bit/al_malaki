4553522bb876513665d988c94bc350818d94cf30
Omar-bit <Bouassidaomar9@gmail.com>
2026-05-19 10:38:37 +0100

    fix: migration sql type

--- server/prisma/migrations/20260506232433_add_category_image_color/migration.sql
+++ server/prisma/migrations/20260506232433_add_category_image_color/migration.sql
@@ -1,3 +1,3 @@
 -- AlterTable
-ALTER TABLE `productcategory` ADD COLUMN `color` VARCHAR(191) NULL,
+ALTER TABLE `ProductCategory` ADD COLUMN `color` VARCHAR(191) NULL,
     ADD COLUMN `image` VARCHAR(191) NULL;
