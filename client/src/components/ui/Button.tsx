0032d3ef2078d8a6045927ea8f37bb111fa86d80
Omar-bit <Bouassidaomar9@gmail.com>
2026-06-13 10:45:57 +0100

    style: fix background scroll on products button images in landing page

--- client/src/components/ui/Button.tsx
+++ client/src/components/ui/Button.tsx
@@ -1,13 +1,35 @@
-import type { ReactNode } from "react";
+import type { ReactNode } from 'react';
 
 type TBackgroundVariant = 'honeyPattern' | 'redPattern';
 
 const bgStyles = {
-    honeyPattern: 'bg-honeyPattern',
-    redPattern: 'bg-redPattern',
+  honeyPattern: 'url(/honey_pattern.png)',
+  redPattern: 'redPattern',
 };
-export default function Button({ classNames, backgroundVariant = 'honeyPattern', children, onClick }: { classNames?: string, backgroundVariant?: TBackgroundVariant, children: ReactNode, onClick?: () => void }) {
-
-    return <button onClick={onClick ? onClick : () => { }} className={`${bgStyles[backgroundVariant]} px-4 py-2 rounded-md text-white font-semibold hover:cursor-pointer px-5 py-2 ` + classNames}>{children}</button>;
+export default function Button({
+  classNames,
+  backgroundVariant = 'honeyPattern',
+  children,
+  onClick,
+}: {
+  classNames?: string;
+  backgroundVariant?: TBackgroundVariant;
+  children: ReactNode;
+  onClick?: () => void;
+}) {
+  return (
+    <button
+      onClick={onClick ? onClick : () => {}}
+      className={
+        ` px-4 py-2 rounded-md text-white font-semibold hover:cursor-pointer px-5 py-2 ` +
+        classNames
+      }
+      style={{
+        backgroundImage: bgStyles[backgroundVariant],
+        backgroundSize: 'cover',
+      }}
+    >
+      {children}
+    </button>
+  );
 }
-
