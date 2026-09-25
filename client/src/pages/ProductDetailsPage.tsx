abdeaeae535cc766fe832f067979aa6fb9c6521f
Omar-bit <Bouassidaomar9@gmail.com>
2026-09-26 00:10:46 +0100

    fix: remove unused Footer import breaking build

--- client/src/pages/ProductDetailsPage.tsx
+++ client/src/pages/ProductDetailsPage.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { Footer, ProductQRCode } from '../components';
+import { ProductQRCode } from '../components';
 import { ArrowLeft } from 'lucide-react';
 import { getPublicProduct } from '../services/productService';
 import { useCart } from '../contexts/CartContext';
